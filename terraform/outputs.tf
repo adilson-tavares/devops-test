4c7fe5155a9107de20af685969c44a7c5b8b2035
adilson-tavares <tavares.adilson.c@gmnail.com>
2025-01-26 07:24:33 -0400

    build: Atualiza outputs TF

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "ec2_id" {
   description = "The ID of the instance"
-  value       = module.ec2.id
+  value       = module.ec2_instance.id
 }
 
 output "ec2_public_dns" {
   description = "The public DNS name assigned to the instance."
-  value       = module.ec2.public_dns
+  value       = module.ec2_instance.public_dns
 }
