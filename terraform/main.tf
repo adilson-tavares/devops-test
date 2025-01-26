1baf2330e546c8757fe1bb8480988f483ae8b240
adilson-tavares <tavares.adilson.c@gmnail.com>
2025-01-26 00:40:09 -0400

    feat: Adiciona terraform

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "test-terraform-at"
+
+  instance_type          = "t2.micro"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
