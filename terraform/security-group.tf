5d8283ae0926c0c5852c2f61fc2aa57916a6af8a
adilson-tavares <tavares.adilson.c@gmnail.com>
2025-01-27 11:55:34 -0400

    chore: Adiciona inboud https

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -17,6 +17,17 @@ resource "aws_security_group" "sg_web_server" {
       self = false
     },
     {
+      description = "HTTPS"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids = []
+      security_groups = []
+      self = false
+    },
+    {
       description = "SSH"
       from_port   = 22
       to_port     = 22
